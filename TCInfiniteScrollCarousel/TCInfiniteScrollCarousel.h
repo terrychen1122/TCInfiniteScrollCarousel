8e7f5385db78e21f071f592bb9df23550e224c13
Xuhui Chen <xuchen@linkedin.com>
2019-07-02 22:24:34 -0700

    Initial Commit

---
+++ TCInfiniteScrollCarousel/TCInfiniteScrollCarousel.h
@@ -0,0 +1,19 @@
+//
+//  TCInfiniteScrollCarousel.h
+//  TCInfiniteScrollCarousel
+//
+//  Created by Xuhui Chen on 7/2/19.
+//  Copyright © 2019 Xuhui Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TCInfiniteScrollCarousel.
+FOUNDATION_EXPORT double TCInfiniteScrollCarouselVersionNumber;
+
+//! Project version string for TCInfiniteScrollCarousel.
+FOUNDATION_EXPORT const unsigned char TCInfiniteScrollCarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCInfiniteScrollCarousel/PublicHeader.h>
+
+
